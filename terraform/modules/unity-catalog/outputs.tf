2c8b71dfa895f34d2ad106d1650bcb8ccd17d7c3
Danish Khan <danishkn2003@outlook.com>
2026-07-17 10:42:02 +0530

    Removed Create Table statement from unity module output

--- terraform/modules/unity-catalog/outputs.tf
+++ terraform/modules/unity-catalog/outputs.tf
@@ -6,9 +6,9 @@ output "schema_name" {
   value = databricks_schema.this.name
 }
 
-output "table_name" {
-  value = databricks_sql_table.this.name
-}
+# output "table_name" {
+#   value = databricks_sql_table.this.name
+# }
 
 output "storage_credential_name" {
   value = databricks_storage_credential.this.name
